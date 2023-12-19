f709ffc1b5d67446cbfc64326ec65087ea99046b
UnmuktShukla <112187110+UnmuktShukla@users.noreply.github.com>
2023-12-19 17:38:47 +0530

    Update PlayerContent.tsx

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -1,15 +1,17 @@
 "use client"
 
-import { Songs } from "@/types";
+import useSound from "use-sound";
+import { useEffect, useState } from "react";
+import { BsPauseFill, BsPlayFill } from "react-icons/bs";
+import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
+import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
+
+import { Song } from "@/types";
+import usePlayer from "@/hooks/usePlayer";
+
 import LikeButton from "./LikeButton";
 import MediaItem from "./MediaItem";
-import {BsPauseFill , BsPlayFill} from "react-icons/bs"
-import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
-import { HiSpeakerXMark, HiSpeakerWave } from "react-icons/hi2"
 import Slider from "./Slider";
-import usePLayer from "@/hooks/usePlayer";
-import { useEffect, useState } from "react";
-import useSound from "use-sound";
 interface PlayerContentProps {
     song: Songs
     songUrl: string
