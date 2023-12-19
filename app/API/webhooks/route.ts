f00a4776a3c077d4b5144e9197ee7dd926a5b080
UnmuktShukla <112187110+UnmuktShukla@users.noreply.github.com>
2023-12-19 19:30:47 +0530

    Update route.ts

--- app/API/webhooks/route.ts
+++ app/API/webhooks/route.ts
@@ -26,7 +26,7 @@ export async function POST(
     const body = await request.text();
     const sig = headers().get('Stripe-Signature');
 
-    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET
+    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET || process.env.STRIPE_WEBHOOK_SECRET;
     let event : Stripe.Event;
 
     try {
